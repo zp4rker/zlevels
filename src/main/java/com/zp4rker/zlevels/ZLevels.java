ff1cdec4bb0497be97fec9b711d251553d1c0bb7
ZP4RKER <ZP4RKER@PROGRAMMER.NET>
2017-03-19 21:12:46 +1100

    Updated version name.

--- src/main/java/com/zp4rker/zlevels/ZLevels.java
+++ src/main/java/com/zp4rker/zlevels/ZLevels.java
@@ -20,7 +20,7 @@ public class ZLevels {
     public static CommandHandler handler;
     public static JDA jda;
 
-    public static final String VERSION = "v1.1";
+    public static final String VERSION = "v1.0.1";
 
     public static void main(String[] args) {
         // Add blank line
