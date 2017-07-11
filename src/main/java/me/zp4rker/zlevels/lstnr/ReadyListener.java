453a50cdf2bd016091101e85a13201fa38cf8fff
ZP4RKER <ZP4RKER@PROGRAMMER.NET>
2017-07-11 16:12:45 +1000

    Added start message.

--- src/main/java/me/zp4rker/zlevels/lstnr/ReadyListener.java
+++ src/main/java/me/zp4rker/zlevels/lstnr/ReadyListener.java
@@ -69,6 +69,9 @@ public class ReadyListener {
 
             ZLogger.info("ZLevels " + ZLevels.VERSION + " is ready!");
         });
+
+        ZLevels.jda.getGuildById(Config.SERVER).getTextChannelById(Config.LOG_CHANNEL)
+                .sendMessage("ZLevels started successfully!").queue();
     }
 
 }
